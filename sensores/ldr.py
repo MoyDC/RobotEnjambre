dff134eb8672d62a7796ec06ecf1de57e9968af1
MoyDC <moises.moy.100@gmail.com>
2024-07-12 09:46:24 -0600

    get adc pins and get command adc has been updated

--- sensores/ldr.py
+++ sensores/ldr.py
@@ -6,12 +6,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../more
 from More_GPIO_ESP32 import MoreGpio_ESP32
 
 # Crear una instancia del módulo MoreGpio_ESP32
-readSensor1 = MoreGpio_ESP32(bus_number=1, address=0x08)
-readSensor2 = MoreGpio_ESP32(bus_number=1, address=0x08)
-
+Sensor1 = MoreGpio_ESP32(bus_number=1, address=0x08)
+Sensor2 = MoreGpio_ESP32(bus_number=1, address=0x08)
 # Define el pin GPIO donde está conectado el sensor (ajústalo según tu configuración)
-pin_sensorLDR1 = MoreGpio_ESP32.ADCPin36
-pin_sensorLDR2 = MoreGpio_ESP32.ADCPin39
+pin_sensorLDR1 = Sensor1.get_adc_pin("ADCPin36")
+pin_sensorLDR2 = Sensor2.get_adc_pin("ADCPin39")
 
 #Queue para almacenar los datos del sensor
 Queue_sensorLDR1 = Queue(maxsize=3)
@@ -27,14 +26,14 @@ def sensorLDR():
             cont += 1
             
             #LDR1
-            sensor1_value, idPin_Return1= readSensor1.readSensor(MoreGpio_ESP32._Command_ADC_, pin_sensorLDR1, cont)
+            sensor1_value, idPin_Return1= Sensor1.readSensor(Sensor1.command_adc, pin_sensorLDR1, cont)
             if sensor1_value is not None:
                 #print(f"{cont} - Sensor 1 Value: {sensor1_value}")
                 datosSensorLDR1 = ("LDR1", sensor1_value)
                 Queue_sensorLDR1.put(datosSensorLDR1)
             
             #LDR 2
-            sensor2_value, idPin_Return2 = readSensor2.readSensor(MoreGpio_ESP32._Command_ADC_, pin_sensorLDR2, cont)
+            sensor2_value, idPin_Return2 = Sensor2.readSensor(Sensor2.command_adc, pin_sensorLDR2, cont)
             if sensor2_value is not None:
                 #print(f"{cont} - Sensor 2 Value: {sensor2_value}")
                 datosSensorLDR2 = ("LDR2", sensor2_value)
