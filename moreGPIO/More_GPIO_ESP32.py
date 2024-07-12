6ad67e94a6f60eff009d6bb85f54bc2f3f5de67a
MoyDC <moises.moy.100@gmail.com>
2024-07-12 13:04:07 -0600

    This is mow a superclass

--- moreGPIO/More_GPIO_ESP32.py
+++ moreGPIO/More_GPIO_ESP32.py
@@ -2,48 +2,53 @@ from smbus2 import SMBus
 import time
 
 class MoreGpio_ESP32:
-    # Atributos 
-    _Command_Output_ = 100
-    _Command_PWM_ = 101
-    _Command_Servomotor_ = 102
-    _Command_ADC_ = 103
-    ADCPin36 = 36
-    ADCPin39 = 39
-    ADCPin34 = 34
-    ADCPin35 = 35
-    
     def __init__(self, bus_number=1, address=0x08):
-        self.bus = SMBus(bus_number)
-        self.address = address
+        self._bus = SMBus(bus_number)
+        self._address = address
+        self._command_output = 100
+        self._command_pwm = 101
+        self._command_servomotor = 102
+        self._command_adc = 103
+
+    @property
+    def command_output(self):
+        return self._command_output
+
+    @property
+    def command_pwm(self):
+        return self._command_pwm
+
+    @property
+    def command_servomotor(self):
+        return self._command_servomotor
+
+    @property
+    def command_adc(self):
+        return self._command_adc
 
     def set_address(self, new_address):
-        self.address = new_address
+        self._address = new_address
 
     def send_command(self, command, pin, value):
         data = [pin, value]
-        # Crear un bloque de datos con el comando y los datos
         block = [command] + data
-        self.bus.write_i2c_block_data(self.address, command, data)
-        #print(f"Data sent: {block}")
+        self._bus.write_i2c_block_data(self._address, command, data)
 
-    def read_command(self, command, pin, value=0,_delay_=0.05):
+    def read_commandAux(self, command, pin, value=0, _delay_=0.05):
         try:
-            self.send_command(command, pin, value)  # Enviar el comando para solicitar los datos del sensor específico
-            time.sleep(_delay_)  # Esperar un momento para que el esclavo procese la solicitud
-            # Leer tres bytes del esclavo
-            data = self.bus.read_i2c_block_data(self.address, 0, 3)
-            # Combinar los bytes en un valor de 16 bits
-            sensor_value = data[0] | (data[1] << 8)
+            self.send_command(command, pin, value)
+            time.sleep(_delay_)
+            data = self._bus.read_i2c_block_data(self._address, 0, 3)
+            value = data[0] | (data[1] << 8)
             idPinReturn = data[2]
-            return sensor_value, idPinReturn
+            return value, idPinReturn
         except Exception as e:
             print(f"I2C communication error: {e}")
             return None
-        
-    def readSensor(self, command, pin, cont, delay=0.01):
+
+    def read_command(self, command, pin_id, ValueSend=0, delay=0.01):
         while True:
-            sensor_value, idPinReturn = self.read_command(command, pin, cont, delay)
-            #print(f"{pin} - {idPinReturn}")
-            if pin == idPinReturn:
-                return sensor_value, idPinReturn
-                break;
+            valueReturn, idPinReturn = self.read_commandAux(command, pin_id, ValueSend, delay)
+            if pin_id == idPinReturn:
+                return valueReturn, idPinReturn
+                break
