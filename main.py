8a89287afd978e5630991fd2b4ad23424d08d605
MoyDC <moises.moy.100@gmail.com>
2024-07-28 20:44:58 -0600

    Using class from sensors module

--- main.py
+++ main.py
@@ -1,124 +1,186 @@
 import time
 import threading
 from threading import Thread
-from sensores.led import control_led
-from sensores.ultrasonicos import leer_sensor, sensores, nombres_sensores, Queue_datosUltrasonicos
-from sensores.lidar import leer_lidar, Queue_datosLidar
-from sensores.infrarrojo import sensorInfrarrojo, Queue_sensorInfrarrojo
-from sensores.brujula import BlujulaDigital, Queue_BlujulaDigital
-from sensores.ldr import sensorLDR, Queue_sensorLDR1, Queue_sensorLDR2
+from sensores.led import LedController
+from sensores.ultrasonicos import UltrasonicSensor
+from sensores.lidar import LidarSensor
+from sensores.infrarrojo import SensorInfrarrojo
+from sensores.brujula import Brujula_MechaQMC5883
+from sensores.ldr import SensorADC
 from actuadores.motor_SpeedController_PID import create_motor_controller, data_queue1, data_queue2
 from actuadores.servomotor import ServoController
 
 #---------------------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------
-
 # Función para formatear y imprimir los datos de los sensores
-def format_sensor_data():
-    contador_datos = 0
-    S1_cm = 0
-    S2_cm = 0
-    S3_cm = 0
-    S4_cm = 0
-    Lidar_cm = 0
-    Infrarrojo = 0
-    AngBrujula = 0
-    SensorLDR1 = 0
-    SensorLDR2 = 0
-    time.sleep(0.1)
-    try:
-        while True:
-            start_time = time.time()
-            time.sleep(0.01)
-            
-            # Lectura de sensores
-            S1_cm = Queue_datosUltrasonicos["S1"].get()  
-            S2_cm = Queue_datosUltrasonicos["S2"].get()
-            S3_cm = Queue_datosUltrasonicos["S3"].get()
-            S4_cm = Queue_datosUltrasonicos["S4"].get()
-            Lidar_cm = Queue_datosLidar.get()
-            Infrarrojo = Queue_sensorInfrarrojo.get();
-            AngBrujula = Queue_BlujulaDigital.get();
-            SensorLDR1 = Queue_sensorLDR1.get();
-            SensorLDR2 = Queue_sensorLDR2.get();
-            
-            # Incrementar contador de datos
-            contador_datos += 1
+class SensorDataFormatter:
+    def __init__(self, sensors, lidar_sensor, sensor_Infrarrojo, sensorBrujula, LDR_Der, LDR_Izq):
+        self.sensors = sensors
+        self.lidar_sensor = lidar_sensor
+        self.sensor_Infrarrojo = sensor_Infrarrojo
+        self.sensorBrujula = sensorBrujula
+        self.LDR_Der = LDR_Der
+        self.LDR_Izq = LDR_Izq
+        self.contador_datos = 0
+        self._is_running = False
+        self._thread = None
 
-            # Imprimir datos formateados
-            formatted_data = (
-                f"Cont {contador_datos}: "
-                f"{nombres_sensores[0]}: {S1_cm:.2f} cm - "
-                f"{nombres_sensores[1]}: {S2_cm:.2f} cm - "
-                f"{nombres_sensores[2]}: {S3_cm:.2f} cm - "
-                f"{nombres_sensores[3]}: {S4_cm:.2f} cm - "
-                f"Lidar: {Lidar_cm[1]} cm - "
-                f"Infra: {Infrarrojo[1]} - "
-                f"Bruj: {AngBrujula[1]:.2f} - "
-                f"LDR1: {SensorLDR1[1]} - "
-                f"LDR2: {SensorLDR2[1]}"
-            )
-            #print(formatted_data)
-            #tamaño = Queue_sensorLDR1.qsize()
-            #print(f"tamaño: {tamaño}")
+    def format_data(self):
+        self.contador_datos += 1
+
+        S1 = self.sensors.get(sensorsNames[0]).get_data()
+        S2 = self.sensors.get(sensorsNames[1]).get_data()
+        S3 = self.sensors.get(sensorsNames[2]).get_data()
+        S4 = self.sensors.get(sensorsNames[3]).get_data()
+        Lidar_cm = self.lidar_sensor.get_data()
+        Infrarrojo = self.sensor_Infrarrojo.get_data()
+        AngBrujula = self.sensorBrujula.get_data()
+        SensorLDR1 = self.LDR_Der.get_data()
+        SensorLDR2 = self.LDR_Izq.get_data()
+
+        formatted_data = (
+            f"Cont {self.contador_datos}: "
+            f"{S1[0]}: {S1[1]:.2f} cm - "
+            f"{S2[0]}: {S2[1]:.2f} cm - "
+            f"{S3[0]}: {S3[1]:.2f} cm - "
+            f"{S4[0]}: {S4[1]:.2f} cm - "
+            f"Lidar: {Lidar_cm[1]} cm - "
+            f"Infra: {Infrarrojo[1]} - "
+            f"Bruj: {AngBrujula[1]:.2f} - "
+            f"LDR1: {SensorLDR1[1]} - "
+            f"LDR2: {SensorLDR2[1]}"
+        )
+        return formatted_data
+
+    def run(self, delay=0.05, Run=True):
+        self._is_running = True
+        time.sleep(0.5)
+        while self._is_running and Run==True:
+            start_time = time.time()
+            formatted_data = self.format_data()
+            print(formatted_data)
+            time.sleep(delay)  # Ajusta el tiempo de espera según sea necesario
             end_time = time.time()
             elapsed_time = end_time - start_time
-            #print(f"Tiempo transcurrido: {elapsed_time} segundos") #Tiempo aproximado 0.05s
-    except KeyboardInterrupt:
-        print("Proceso de impresión de datos finalizado.")     
+            #print(f"Tiempo transcurrido: {elapsed_time} segundos")  # Tiempo aproximado 0.05s
+
+    def start(self):
+        if not self._thread or not self._thread.is_alive():
+            self._thread = threading.Thread(target=self.run)
+            self._thread.start()
+
+    def stop(self):
+        self._is_running = False
+        if self._thread:
+            self._thread.join()
+        print("Proceso de recopilación de datos detenido.")
+        
+        
 
 #---------------------------------------------------------------------------------------------------------------
         
 # Funciona para inicializar todos los hilos
 threads = [] # Lista global para almacenar referencias de hilos
 def init_threads():
-    controlSpeed_thread1 = threading.Thread(target=motor1_speedController.control_loop, args=(data_queue1,))
-    controlSpeed_thread2 = threading.Thread(target=motor2_speedController.control_loop, args=(data_queue2,))
+    thread_controlSpeed1 = threading.Thread(target=motor1_speedController.control_loop, args=(data_queue1,))
+    thread_controlSpeed2 = threading.Thread(target=motor2_speedController.control_loop, args=(data_queue2,))
 
-    controlSpeed_thread1.start()
-    controlSpeed_thread2.start()
-    threads.extend([controlSpeed_thread1, controlSpeed_thread2])
+    thread_controlSpeed1.start()
+    thread_controlSpeed2.start()
+    threads.extend([thread_controlSpeed1, thread_controlSpeed2])
     
      # Iniciar el hilo del LED
-    thread_led = Thread(target=control_led)
-    thread_led.start()
-    threads.append(thread_led)
+    thread_ledPrograma= threading.Thread(target=Led_Programa.blink)
+    thread_ledPrograma.start()
+    threads.append(thread_ledPrograma)
     
     # Iniciar el hilo de lectura del sensor lidar
-    thread_lidar = Thread(target=leer_lidar)
+    thread_lidar = Thread(target=lidar_sensor.start_reading)
     thread_lidar.start()
     threads.append(thread_lidar)
     
-    time.sleep(1)
+    time.sleep(0.5)
     
-    # Iniciar los hilos de lectura de sensores ultrasónicos
-    for sensor, nombre_sensor in zip(sensores, nombres_sensores):
-        thread_sensor = Thread(target=leer_sensor, args=(sensor, nombre_sensor))
-        thread_sensor.start()
-        threads.append(thread_sensor)
+    # Iniciar hilos para leer los datos de los sensores
+    for name, sensor in sensors.items():
+        thread = Thread(target=sensor.start_reading)
+        thread.start()
+        threads.append(thread)
+    
+    time.sleep(0.5)
     
     # Iniciar el hilo de lectura del sensor infrarrojo
-    thread_sensorInfrarrojo = Thread(target=sensorInfrarrojo)
+    thread_sensorInfrarrojo = Thread(target=sensor_Infrarrojo.start_reading)
     thread_sensorInfrarrojo.start()
     threads.append(thread_sensorInfrarrojo)
     
     # Iniciar el hilo de lectura de la brujula digital
-    thread_BlujulaDigital = Thread(target=BlujulaDigital)
+    thread_BlujulaDigital = Thread(target=sensorBrujula.start_reading)
     thread_BlujulaDigital.start()
     threads.append(thread_BlujulaDigital)
     
     # Iniciar el hilo de lectura del sensor LDR
-    thread_sensorLDR = Thread(target=sensorLDR)
-    thread_sensorLDR.start()
-    threads.append(thread_sensorLDR)
+    thread_sensorLDR1 = Thread(target=LDR_Der.start)
+    thread_sensorLDR2 = Thread(target=LDR_Izq.start)
+    
+    thread_sensorLDR1.start()
+    thread_sensorLDR2.start()
+    #threads.append(thread_sensorLDR1)
+    threads.extend([thread_sensorLDR1, thread_sensorLDR2])
     
     # Iniciar el hilo de impresión de datos
-    thread_print_data = Thread(target=format_sensor_data)
+    thread_print_data = Thread(target=PrintDataSensors.start)
     thread_print_data.start()
     threads.append(thread_print_data)
 
 #---------------------------------------------------------------------------------------------------------------
 #---------------------------------------------------------------------------------------------------------------
+
+# Crear una instancia de LedController para el led Programa
+pinLed = 18
+delayBlink = 0.25
+Led_Programa = LedController(pinLed, delayBlink)
+    
+#---------------------------------------------------------------------------------------------------------------
+
+#Crear estancia del sensor Lidar
+lidar_sensor = LidarSensor(serial_port="/dev/ttyAMA0", baud_rate=115200, max_queue_size=3)
+lidar_sensor.initialize(frame_rate=50)
+
+#---------------------------------------------------------------------------------------------------------------
+
+# Configuración de los sensores Ultrasonicos
+sensorsNames = ["S1", "S2", "S3", "S4"]
+sensor_configs = [
+    {"name": sensorsNames[0], "echo": 16, "trigger": 26},
+    {"name": sensorsNames[1], "echo": 5, "trigger": 6},
+    {"name": sensorsNames[2], "echo": 7, "trigger": 1},
+    {"name": sensorsNames[3], "echo": 25, "trigger": 8}
+]
+# Crear instancias de los sensores y almacenarlas en un diccionario
+sensors = {config['name']: UltrasonicSensor(config['echo'], config['trigger'], config['name']) for config in sensor_configs}
+
+#---------------------------------------------------------------------------------------------------------------
+
+# Inicializar el sensor infrarrojo
+sensor_Infrarrojo = SensorInfrarrojo(pin=20)
+
+#---------------------------------------------------------------------------------------------------------------
+
+sensorBrujula = Brujula_MechaQMC5883()
+sensorBrujula.init()
+
+#---------------------------------------------------------------------------------------------------------------
+
+# Configuración de los sensores
+LDR_Der = SensorADC(bus_number=1, address=0x08, pin_sensor=36)
+LDR_Izq = SensorADC(bus_number=1, address=0x08, pin_sensor=39)
+
+#---------------------------------------------------------------------------------------------------------------
+
+PrintDataSensors = SensorDataFormatter(sensors, lidar_sensor, sensor_Infrarrojo, sensorBrujula, LDR_Der, LDR_Izq)
+
+#---------------------------------------------------------------------------------------------------------------  
     
 # Configuración del sistema de control de motores usando PID
 Kp = 2
@@ -156,6 +218,7 @@ if __name__ == "__main__":
     try:
         #Inicializar todos los hilos
         init_threads()
+        #
         
         # Configuracion inicial motores
         motor1_speedController.set_setpoint(150)
@@ -165,6 +228,8 @@ if __name__ == "__main__":
         servo1.setup_servo(servo1.get_servo_name_by_pin(pinServo1), 10)
         servo2.setup_servo(servo2.get_servo_name_by_pin(pinServo2), 150)
         servo3.setup_servo(servo3.get_servo_name_by_pin(pinServo3), 100)
+        
+        PrintDataSensors.stop()
 
         while True:
             if not data_queue1.empty() and not data_queue2.empty():
@@ -177,9 +242,20 @@ if __name__ == "__main__":
             time.sleep(0.1)
             
     except KeyboardInterrupt:
-        print("Interrupción recibida, deteniendo todos los hilos...")
-        motor1_speedController.set_setpoint(0)
-        motor2_speedController.set_setpoint(0)
+        print("\nInterrupción recibida (Ctrl + C), deteniendo todos los hilos...")
+        PrintDataSensors.stop()
+        Led_Programa.stop()
+        lidar_sensor.stop_reading()
+        sensors.get(sensorsNames[0]).stop()
+        sensors.get(sensorsNames[1]).stop()
+        sensors.get(sensorsNames[2]).stop()
+        sensors.get(sensorsNames[3]).stop()
+        sensor_Infrarrojo.stop()
+        sensorBrujula.stop()
+        LDR_Der.stop()
+        LDR_Izq.stop()
+        motor1_speedController.stop(name="Motor 1")
+        motor2_speedController.stop(name="Motor 2")
         
     finally:
         for thread in threads:
